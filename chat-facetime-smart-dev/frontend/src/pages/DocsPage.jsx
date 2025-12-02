b1164bdd4930223a2fd429a9669f521f07e1a075
ADMIN-PC\Admin <nguyenhoangle070105@gmail.com>
2025-12-02 10:28:22 +0700

    Cập nhật DocPages

--- chat-facetime-smart-dev/frontend/src/pages/DocsPage.jsx
+++ chat-facetime-smart-dev/frontend/src/pages/DocsPage.jsx
@@ -1,16 +1,19 @@
 import HeaderDocs from "../components/HeaderDocs";
-// import SidebarDocs from "../components/SidebarDocs";
-// import ContentDocs from "../components/ContentDocs";
-import React from "react";
+import React, { useState } from "react";
 import SidebarDocs from "../components/SidebarDocs";
 import DocsContent from "../components/DocContent";
 
 function DocsPage() {
+  const [currentSection, setCurrentSection] = useState("overview");
+
   return (
     <div className="flex flex-col min-h-screen">
       <HeaderDocs />
-      <SidebarDocs />
-      <DocsContent />
+      <SidebarDocs 
+        currentSection={currentSection} 
+        setCurrentSection={setCurrentSection} 
+      />
+      <DocsContent currentSection={currentSection} />
     </div>
   );
 }
